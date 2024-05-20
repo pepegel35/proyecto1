49b57d77a5e102108f053b6f7ad3a038ac0571d2
EpicBro1 <sabs355346@gmail.com>
2024-05-19 20:58:23 -0600

    Codigo Documentado

--- static/js/app.js
+++ static/js/app.js
@@ -31,17 +31,17 @@ particlesJS(
       "random": false,
       "anim": {
         "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
+        "speed": 1.4,
+        "opacity_min": 0.5,
         "sync": false
       }
     },
     "size": {
-      "value": 3,
+      "value": 4,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 40,
+        "speed": 30,
         "size_min": 0.1,
         "sync": false
       }
@@ -50,12 +50,12 @@ particlesJS(
       "enable": true,
       "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.4,
+      "opacity": 0.6,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 6.4,
       "direction": "none",
       "random": false,
       "straight": false,
